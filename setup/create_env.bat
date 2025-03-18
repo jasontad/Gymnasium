1881f9df0ed89286eea7303529df69ee01ee8432
Jason <jason.tad@icloud.com>
2025-03-18 18:10:44 +0900

    [Update] setup scripts

--- setup/create_env.bat
+++ setup/create_env.bat
@@ -1,16 +1,12 @@
 @echo off
 
-rem Name of your conda environment
-set ENV_NAME=gymnasium-env
-
 rem Create the conda environment with Python 3.10
-conda create -y -n %ENV_NAME% python=3.10
+conda create -y -n gymnasium-env python=3.10
 
 rem Activate the environment
-call conda activate %ENV_NAME%
+conda activate gymnasium-env
 
 rem Install dependencies from requirements.txt
 pip install -r requirements.txt
 
-rem Run the batch file 
-rem create_env.bat
+pause
--- setup/create_env.sh
+++ setup/create_env.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Name of your conda environment
-ENV_NAME="gymnasium-env"
-
 # Create the conda environment with Python 3.10
-conda create -y -n $ENV_NAME python=3.10
+conda create -y -n gymnasium-env python=3.10
 
 # Activate the environment
 source $(conda info --base)/etc/profile.d/conda.sh
-conda activate $ENV_NAME
+conda activate gymnasium-env
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
